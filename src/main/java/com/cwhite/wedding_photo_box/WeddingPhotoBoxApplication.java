eafed25f4f1c3dd32f435ed9b5a89b2223ce51d1
cwhite56 <cam.white1117@gmail.com>
2026-05-14 16:38:12 -0400

    First commit

---
+++ src/main/java/com/cwhite/wedding_photo_box/WeddingPhotoBoxApplication.java
@@ -0,0 +1,13 @@
+package com.cwhite.wedding_photo_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPhotoBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingPhotoBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwhite/wedding_photo_box/WeddingPhotoBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwhite.wedding_photo_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPhotoBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
