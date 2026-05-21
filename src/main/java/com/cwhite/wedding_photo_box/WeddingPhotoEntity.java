6896294e9a0265e6ebc898de744803202e655eae
cwhite56 <cam.white1117@gmail.com>
2026-05-21 16:12:52 -0400

    POST method working with postman

--- src/main/java/com/cwhite/wedding_photo_box/WeddingPhotoEntity.java
+++ src/main/java/com/cwhite/wedding_photo_box/WeddingPhotoEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,7 +20,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "photos")
 public class WeddingPhotoEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "wedding_photo_id_seq")
+    @SequenceGenerator(name = "wedding_photo_id_seq", sequenceName = "wedding_photo_id_seq", allocationSize = 1)
     private Long id;
 
     private String sentBy;
