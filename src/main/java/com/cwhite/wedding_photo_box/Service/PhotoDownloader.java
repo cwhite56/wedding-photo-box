fcbe187e3ff32c9f7548c146f2b7aecff3802fab
cwhite56 <cam.white1117@gmail.com>
2026-06-10 23:59:42 -0400

    Working on zip  fnf error

--- src/main/java/com/cwhite/wedding_photo_box/Service/PhotoDownloader.java
+++ src/main/java/com/cwhite/wedding_photo_box/Service/PhotoDownloader.java
@@ -1,6 +1,10 @@
 package com.cwhite.wedding_photo_box.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -10,6 +14,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
 
@@ -29,11 +35,38 @@ public class PhotoDownloader {
 
     private static final String BUCKET_NAME = "amzn-s3-wedding-photo-box-2027";
 
+    private static final String ZIP_PATH = "home/cwhite56/Pictures/";
+
+    private static final String ZIP_FILE = "/home/cwhite56/Pictures/wedding-photos.zip";
+
     public static void main(String[]args) throws SQLException, IOException {
         List<String> urlList = createUrlList();
         System.out.println(urlList);
         System.out.println("Found " + urlList.size() + " photos in database.");
-        downloadFromS3(urlList);
+        List<String> fileList = downloadFromS3(urlList);
+        zipFiles(fileList);
+    }
+
+    private static void zipFiles(List<String> fileList) throws IOException {
+        FileOutputStream os = new FileOutputStream(ZIP_FILE);
+        ZipOutputStream zs = new ZipOutputStream(os);
+
+        byte[] buffer = new byte[1024];
+
+        for (String file : fileList) {
+            File fileToZip = new File(ZIP_PATH + file);
+            
+            FileInputStream is = new FileInputStream(fileToZip);
+
+            zs.putNextEntry(new ZipEntry(fileToZip.getName()));
+
+            int length;
+
+            while((length = is.read(buffer)) > 0) {
+                zs.write(buffer, 0, length);
+            }
+            zs.closeEntry();
+        }
     }
 
     private static List<String> downloadFromS3(List<String> urlList) throws IOException {
@@ -55,10 +88,17 @@ public class PhotoDownloader {
             GetObjectResponse getObjectResponse = responseInputStream.response();
 
             String key = getObjectRequest.key();
-            Files.copy(responseInputStream, Paths.get("/home/cwhite56/Pictures/" + key));
-            fileNames.add(key);
+            try {
+                Files.copy(responseInputStream, Paths.get("/home/cwhite56/Pictures/" + key));
+            }
 
-            //byte[] fileContent = IOUtils.toByteArray(responseInputStream);
+            catch (FileAlreadyExistsException e) {
+                //e.printStackTrace();
+            }
+
+            finally {
+                fileNames.add(key);
+            }
             
         }
         return fileNames;
